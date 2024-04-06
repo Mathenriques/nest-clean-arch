060010627e138a7a2f1be309f69b795c33384f82
Mathenriques <math.marqui@gmail.com>
2024-04-05 23:06:31 -0300

    Arrumando Controller Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,19 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Request,
   UseGuards,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest.model';
 
 @Controller()
 export class AuthController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  login() {
-    return 'Realizar Login';
+  login(@Request() req: AuthRequest) {
+    console.log(req.user);
+    // return 'Realizar Login';
   }
 }
